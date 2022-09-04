44f0887b17611afe9f27eeb0bf838499f7336243
Shoabe Shariff <shoabe@tryfinch.com>
2022-09-03 21:52:00 -0700

    Register and implement /get for reading from queue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'; //Import the express dependency
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import SqsClient from './lib/sqsClient.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,10 +9,17 @@ const __dirname = dirname(__filename);
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 3333;                  //Save the port number where your server will be listening
 
+const sqsClient = new SqsClient();
+
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
+});
+
+app.get('/get', async (req, res) => {
+    const response = await sqsClient.readMessageFromQueue('Jobstream-v0');
+    res.status(200).send(response);
 });
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
