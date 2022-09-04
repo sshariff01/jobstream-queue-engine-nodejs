b8e38d26d36037d0b455799fb94ec8bb91930ba5
Shoabe Shariff <shoabe@tryfinch.com>
2022-09-03 21:44:19 -0700

    Move to using Node.js modules. Move logger init to its own file in init/ folder.

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
-const express = require('express'); //Import the express dependency
-const winston = require('winston');
+import express from 'express'; //Import the express dependency
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-/*** Init logger ***/
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'log/combined.log' }),
-  ],
-});
-/*** logger ***/
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 3333;                  //Save the port number where your server will be listening
